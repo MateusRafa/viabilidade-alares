e373f47d8b7465ae64db07b89d47aac868f65bda
MateusRafa <medeirosmonte18@gmail.com>
2026-05-30 09:18:14 -0300

    Update App.svelte

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -958,7 +958,7 @@
         onBackToDashboard={handleBackToDashboard}
         onOpenSettings={handleOpenSettings}
         onSettingsHover={handleSettingsHover}
-        showSettingsButton={toolSettingsHandler !== null && tool.id !== 'analise-cobertura' && tool.id !== 'viabilidade-alares' && tool.id !== 'formulario-engenharia' && tool.id !== 'dashboard-projetos' && tool.id !== 'dashboard-implantacao'}
+        showSettingsButton={toolSettingsHandler !== null && tool.id !== 'analise-cobertura' && tool.id !== 'viabilidade-alares' && tool.id !== 'formulario-engenharia' && tool.id !== 'formulario-engenharia-implantacao' && tool.id !== 'dashboard-projetos' && tool.id !== 'dashboard-implantacao'}
       >
         <svelte:component this={tool.component} 
           currentUser={currentUser}
